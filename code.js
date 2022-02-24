936cfe56aa41aae1043b44e6cfaeb71b6f62c67f
Sofía Villazón <sofia.villazon@kushkipagos.com>
2022-02-24 14:41:02 -0500

    Ejemplo meetup

---
+++ code.js
@@ -0,0 +1,27 @@
+figma.showUI(__html__);
+figma.ui.resize(320, 640);
+// Receive the drop event from the UI
+figma.on('drop', (event) => {
+    const { files, node, dropMetadata } = event;
+    if (files.length > 0 && files[0].type === 'image/svg+xml') {
+        files[0].getTextAsync().then(text => {
+            if (dropMetadata.parentingStrategy === 'page') {
+                const newNode = figma.createNodeFromSvg(text);
+                newNode.x = event.absoluteX;
+                newNode.y = event.absoluteY;
+                figma.currentPage.selection = [newNode];
+            }
+            else if (dropMetadata.parentingStrategy === 'immediate') {
+                const newNode = figma.createNodeFromSvg(text);
+                if (node[0].appendChild) {
+                    node[0].appendChild(newNode);
+                    node[0].appendChild(newNode);
+                }
+                newNode.x = event.x;
+                newNode.y = event.y;
+                figma.currentPage.selection = [newNode];
+            }
+        });
+        return false;
+    }
+});
